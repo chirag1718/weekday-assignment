387f05a13309069a0d820ef9d71049151b96e5ff
chirag sonar <chiragsonar1718@gmail.com>
2024-05-08 03:05:42 +0530

    chore: added new reducer filterSlice

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "./features/modalSlice";
 import jobPostSlice from "./features/jobPostSlice";
+import fliterSlice from "./features/fliterSlice";
 
 export const store = configureStore({
     reducer: {
         modal: modalReducer,
         jobPosts: jobPostSlice,
+        filters: fliterSlice,
     },
 });
 
