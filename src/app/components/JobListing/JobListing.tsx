4ec04c594c237714766b14838b5e22fee8844257
chirag sonar <chiragsonar1718@gmail.com>
2024-05-08 03:05:17 +0530

    feat: filtering data and rendering Filters component

--- src/app/components/JobListing/JobListing.tsx
+++ src/app/components/JobListing/JobListing.tsx
@@ -3,12 +3,14 @@ import { RootState } from '@/app/redux/store'
 import { Grid, Box } from '@mui/material'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import jobPostSlice, { setIsLoading, setPosts, setHasMorePosts } from '@/app/redux/features/jobPostSlice'
-import JobCard from '../JobCard/JobCard'
+import { setIsLoading, setPosts, setHasMorePosts } from '@/app/redux/features/jobPostSlice'
+import JobCard, { PostsArray } from '../JobCard/JobCard'
+import Filters from '../Filters/Filters'
 
 const JobListing = () => {
      const dispatch = useDispatch()
      const { posts, isLoading, hasMorePosts } = useSelector((state: RootState) => state.jobPosts)
+     const { companyName, jobType, location, minBasePay, minExperience, role } = useSelector((state: RootState) => state.filters)
      useEffect(() => {
           handleFetchJobPosts()
      }, [])
@@ -33,7 +35,6 @@ const JobListing = () => {
                dispatch(setIsLoading(true)) // isloading = true
                const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions) // fetching posts
                const result = await response.json()//
-               // console.log(result)
                dispatch(setPosts(result.jdList)) // setting posts
                dispatch(setIsLoading(false)) // isLoading = false
 
@@ -48,15 +49,21 @@ const JobListing = () => {
           }
      }
 
-
-     useEffect(() => {
-          console.log(posts)
-     }, [posts])
+     //? refactor this code into a hook
+     const filteredPosts = posts?.filter((post: PostsArray) => {
+          const roleMatch = !role || post.jobRole.toLowerCase().includes(role.toLowerCase()) // filters for job role
+          const locationMatch = !location || post.location.toLowerCase().includes(location.toLowerCase()) // filter for locations 
+          const companyNameMatch = !companyName || post.companyName.toLowerCase().includes(companyName.toLowerCase()) // instant search filter for company names
+          const jobTypeMatch = !jobType || post.location.toLowerCase().includes(jobType.toLowerCase()) // filters for job type
+          const minExpMatch = minExperience === "" || (post.minExp !== null && post.minExp === parseInt(minExperience)); // exactly filters out for specific year
+          const minSalaryMatch = minBasePay === "" || (post.minJdSalary !== null && post.minJdSalary >= parseInt(minBasePay)); // filter salary from X LPA and above for better rendering of results
+          return roleMatch && locationMatch && companyNameMatch && jobTypeMatch && minExpMatch && minSalaryMatch
+     })
      return (
           <Box className='listing-wrapper'>
-               <Grid container spacing={8} marginBottom={10} direction="row" justifyContent={"center"} alignItems={"center"} className='listing-wrapper' sx={{ width: "100%", height: "100%" }}>
-                    {posts.map((post, index) => {
-                         console.log(index)
+               <Filters />
+               <Grid container gap={5} margin={0} marginBottom={10} direction="row" justifyContent={"center"} alignItems={"center"} className='listing-wrapper' width={"100%"} height={"100%"}>
+                    {filteredPosts.map((post, index) => {
                          return (
                               <Grid key={`${post.jdUid}_${index}`} item >
                                    <JobCard {...post} />
