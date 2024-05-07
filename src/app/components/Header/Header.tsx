7ce36c7fa3ce207e9abad8d69d59149463fab9ef
chirag sonar <chiragsonar1718@gmail.com>
2024-05-07 17:01:34 +0530

    feat: created header component

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+     return (
+          <div className='header'>
+               <p>Weekday Works</p>
+               {/* <p>&gt;</p>
+               <p className='link'>Firefly</p>
+               <p>&gt;</p>
+               <p className='link'>Frontend Engineer</p> */}
+          </div>
+     )
+}
+
+export default Header
