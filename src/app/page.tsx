ffe4da0c2d00f66a83d368632f3b2671fe91f51f
chirag sonar <chiragsonar1718@gmail.com>
2024-05-07 17:00:39 +0530

    feat: rendering joblisting and header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Header from "./components/Header/Header";
 import JobCard from "./components/JobCard/JobCard";
-
+import JobListing from "./components/JobListing/JobListing";
+import "./page.css"
 export default function Home() {
   return (
-    <div>
-      <JobCard />
+    <div className="app">
+      <Header />
+      <JobListing />
     </div>
   );
 }
