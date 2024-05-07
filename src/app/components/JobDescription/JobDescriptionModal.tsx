80e1ebc7e9433aaa89f4ea88cc6ab8e8b3e3ebfc
chirag sonar <chiragsonar1718@gmail.com>
2024-05-07 10:34:56 +0530

    chore: added boilerplate code for Job description modal component

---
+++ src/app/components/JobDescription/JobDescriptionModal.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { toggleModal } from '@/app/redux/features/modalSlice'
+import { Box } from '@mui/material'
+import Modal from '@mui/material/Modal'
+import React from 'react'
+
+const JobDescriptionModal = () => {
+  return (
+    <div>
+      {/* <Modal
+        // open={isOpen}
+        // onClose={handleToggleModal}
+      >
+        <Box>asdas</Box>
+      </Modal> */}
+    </div>
+  )
+}
+
+export default JobDescriptionModal
