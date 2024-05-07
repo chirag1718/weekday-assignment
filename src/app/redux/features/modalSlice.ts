33b28d8a7180869e7e98a55d31ec2922efb56a97
chirag sonar <chiragsonar1718@gmail.com>
2024-05-08 03:06:58 +0530

    feat: added jobId to open modals for specific job

--- src/app/redux/features/modalSlice.ts
+++ src/app/redux/features/modalSlice.ts
@@ -2,18 +2,21 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface ModalState {
     isOpen: boolean;
+    jobId: string | null;
 }
 
 const initialState: ModalState = {
     isOpen: false,
+    jobId: null,
 };
 
 const modalState = createSlice({
     name: "modal",
     initialState,
     reducers: {
-        toggleModal(state) {
+        toggleModal(state, action) {
             state.isOpen = !state.isOpen;
+            state.jobId = action.payload;
         },
     },
 });
