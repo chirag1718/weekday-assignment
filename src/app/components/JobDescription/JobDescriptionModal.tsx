e44ec1f97a480a118613ac00aeea9496e097183c
chirag sonar <chiragsonar1718@gmail.com>
2024-05-08 03:02:37 +0530

    feat: conditionally checking for job id and added more details about the company and job in the modal

--- src/app/components/JobDescription/JobDescriptionModal.tsx
+++ src/app/components/JobDescription/JobDescriptionModal.tsx
@@ -6,31 +6,40 @@ import Modal from '@mui/material/Modal'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import "./JobDescription.css"
-const JobDescriptionModal = ({ jobDes }: { jobDes: string }) => {
-  const { isOpen } = useSelector((state: RootState) => state.modal)
+const JobDescriptionModal = ({ jobDes, jobRole, companyName, jobLocation, id }: { jobDes: string, jobRole: string, companyName: string, jobLocation: string, id: string }) => {
+  const { isOpen, jobId } = useSelector((state: RootState) => state.modal)
 
   const dispatch = useDispatch<AppDispatch>()
   const handleToggleModal = () => {
-    dispatch(toggleModal())
+    dispatch(toggleModal(null))
   }
   return (
     <React.Fragment>
-      {isOpen &&
-        <Modal
-
-          className="modal-wrapper"
-          open={isOpen}
-          onClose={handleToggleModal}
-        >
-          <Fade in={isOpen}>
-            <Box className='modal-container'>
-              {jobDes}
-            </Box>
-          </Fade>
-        </Modal>
+      {isOpen && id === jobId &&
+        <Box sx={{ backgroundColor: "transparent" }}>
+          <Modal
+            className="modal-wrapper"
+            open={isOpen}
+            onClick={handleToggleModal}
+          >
+            <Fade in={isOpen} >
+              <div className='modal-container'>
+                <p className='title'>Job Description</p>
+                <div className='details'>
+                  <p>{companyName} | {jobRole} | {jobLocation}</p>
+                </div>
+                <div>
+                  <p className='sub-title'>About Company:</p>
+                  <p className='job-description'>{jobDes}</p>
+                </div>
+              </div>
+            </Fade>
+          </Modal>
+        </Box>
       }
     </React.Fragment>
   )
 }
 
 export default JobDescriptionModal
+
