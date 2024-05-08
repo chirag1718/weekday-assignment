4b293a914357690458a7eb316d8a775c7987edce
chirag sonar <chiragsonar1718@gmail.com>
2024-05-08 10:52:34 +0530

    fix: removed unwanted code

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,10 +4,6 @@ const Header = () => {
      return (
           <div className='header'>
                <p>Weekday Works</p>
-               {/* <p>&gt;</p>
-               <p className='link'>Firefly</p>
-               <p>&gt;</p>
-               <p className='link'>Frontend Engineer</p> */}
           </div>
      )
 }
